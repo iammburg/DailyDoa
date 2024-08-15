af5fa0518ba209cd5c0dafd2c04865c82e6fc8df
iammburg <ilhamprabowo009@gmail.com>
2024-08-16 01:04:10 +0700

    fix: navbar max-wodth and button fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,86 +53,88 @@ export function StickyNavbar() {
   );
 
   return (
-    <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
-      <div className="flex items-center justify-between text-blue-gray-900">
-        <Typography
-          as="a"
-          href="#"
-          className="mr-4 flex cursor-pointer flex-row items-center justify-center gap-2 py-1.5 font-cardo text-xl font-extrabold"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="size-6"
+    <div className="flex items-center justify-center">
+      <Navbar className="max-w-screen-xxl sticky top-0 z-10 h-max rounded-none px-4 py-2 lg:px-8 lg:py-4">
+        <div className="flex items-center justify-between text-blue-gray-900">
+          <Typography
+            as="a"
+            href="#"
+            className="mr-4 flex cursor-pointer flex-row items-center justify-center gap-2 py-1.5 font-cardo text-xl font-extrabold"
           >
-            <path
-              fillRule="evenodd"
-              d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
-              clipRule="evenodd"
-            />
-          </svg>{" "}
-          DailyDoa
-        </Typography>
-        <div className="mr-4 hidden items-center justify-center lg:block">
-          {navList}
-        </div>
-        <div className="flex items-center justify-center gap-4">
-          <div className="flex items-center gap-x-1">
-            <Button
-              size="md"
-              className="hidden bg-color-jungle lg:inline-block"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="size-6"
             >
+              <path
+                fillRule="evenodd"
+                d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
+                clipRule="evenodd"
+              />
+            </svg>{" "}
+            DailyDoa
+          </Typography>
+          <div className="mr-4 hidden items-center justify-center lg:block">
+            {navList}
+          </div>
+          <div className="flex items-center justify-center gap-4">
+            <div className="flex items-center gap-x-1">
+              <Button
+                size="md"
+                className="hidden bg-color-jungle lg:inline-block"
+              >
+                <span>Kontak</span>
+              </Button>
+            </div>
+            <IconButton
+              variant="text"
+              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              ripple={false}
+              onClick={() => setOpenNav(!openNav)}
+            >
+              {openNav ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </IconButton>
+          </div>
+        </div>
+        <MobileNav open={openNav}>
+          {navList}
+          <div className="flex items-center gap-x-1">
+            <Button fullWidth size="sm" className="bg-color-jungle">
               <span>Kontak</span>
             </Button>
           </div>
-          <IconButton
-            variant="text"
-            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-            ripple={false}
-            onClick={() => setOpenNav(!openNav)}
-          >
-            {openNav ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                className="h-6 w-6"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            )}
-          </IconButton>
-        </div>
-      </div>
-      <MobileNav open={openNav}>
-        {navList}
-        <div className="flex items-center gap-x-1">
-          <Button size="sm" className="hidden bg-color-jungle lg:inline-block">
-            <span>Kontak</span>
-          </Button>
-        </div>
-      </MobileNav>
-    </Navbar>
+        </MobileNav>
+      </Navbar>
+    </div>
   );
 }
