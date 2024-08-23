93bb184e9799b60b522355d99ceb2f8fa9276e6e
iammburg <ilhamprabowo009@gmail.com>
2024-08-23 21:38:59 +0700

    feat: add about section

---
+++ src/components/Card.jsx
@@ -0,0 +1,29 @@
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
+
+export function SimpleCard({ cardHeader, cardContent, buttonName }) {
+  return (
+    <Card className="mt-6 flex h-96 w-[300px] items-center justify-center border border-color-jungle shadow-2xl">
+      <CardBody>
+        <Typography
+          variant="h5"
+          color="blue-gray"
+          className="mb-2 text-center font-cardo"
+        >
+          {cardHeader}
+        </Typography>
+        <Typography className="text-center font-amiri" variant="paragraph">
+          {cardContent}
+        </Typography>
+      </CardBody>
+      <CardFooter className="flex items-center justify-center pt-0">
+        <Button>{buttonName}</Button>
+      </CardFooter>
+    </Card>
+  );
+}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,46 @@
+import { Typography } from "@material-tailwind/react";
+import { SimpleCard } from "../components/Card";
+import "../styles/about.css";
+
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="about mb-10 mt-10 w-full">
+      <div className="about__container container mx-auto flex max-w-screen-xxl flex-col items-center justify-center lg:pt-8">
+        <div className="about__text flex flex-col items-center justify-center lg:px-16">
+          <Typography
+            variant="h2"
+            className="text-center font-cardo text-2xl leading-none text-color-abyss lg:text-start xl:text-3xl"
+          >
+            Hadist Tentang Doa
+          </Typography>
+          <Typography
+            variant="paragraph"
+            className="mt-3 text-center font-amiri text-lg leading-none text-color-abyss xl:text-xl"
+          >
+            Pelajari hadist-hadist yang mendasari doa-doa harian kita. Dapatkan
+            pemahaman lebih dalam tentang pentingnya doa dalam kehidupan
+            sehari-hari dan bagaimana doa dapat memberikan kekuatan serta
+            kedamaian batin.
+          </Typography>
+        </div>
+        <div className="about__card mt-10 flex flex-col items-center justify-center gap-6 md:flex-row md:flex-wrap md:items-start md:gap-9 lg:gap-16">
+          <SimpleCard
+            cardHeader={`Dari An-Nu'man bin Basyir-raḍiyallāhu'anhu:`}
+            cardContent={`Nabi (ﷺ) bersabda, 'Doa (permohonan) adalah ibadah.'`}
+            buttonName={`Referensi`}
+          />
+          <SimpleCard
+            cardHeader={`Aisyah-raḍiyallāhu'anhā meriwayatkan:`}
+            cardContent={`Rasulullah (ﷺ) menyukai doa yang komprehensif atau (Al-Jawami', yaitu, doa dengan kata-kata yang sangat sedikit tetapi memiliki makna yang luas), dan membuang yang lainnya.'`}
+            buttonName={`Referensi`}
+          />
+          <SimpleCard
+            cardHeader={`Anas radhiyallahu'anhu meriwayatkan:`}
+            cardContent={`Doa yang paling sering diucapkan oleh Nabi (ﷺ) adalah: 'Allahumma atina fid-dunya hasanatan, wa fil-akhirati hasanatan, wa qina 'adhab-annar (Wahai Rubb kami, berilah kami di dunia ini apa yang baik, dan di akhirat nanti apa yang baik, dan selamatkanlah kami dari azab api neraka).'`}
+            buttonName={`Referensi`}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
